37d36a8fdac1808480b4f7fed6bd4acd1d3a7c56
acoache <anthony.coache@gmail.com>
2026-08-10 09:19:43 +0100

    Initial push

---
+++ src/summarytools/ctable.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from .summarytools import _var_name
+import numpy as np
+from IPython.display import HTML
+from .htmlwidgets import collapsible
+
+def ctable():
+    """generate cross-tabulations (joint frequencies) for pairs of categorical variables
+    """
+    pass
